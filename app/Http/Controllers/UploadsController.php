73dff6131b14765ff73e852ce40579e4af042d00
Maz <mohammad6897@gmail.com>
2020-05-24 12:02:46 +0430

    Making Image Upload

---
+++ app/Http/Controllers/UploadsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ImageUpload;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
+class UploadsController extends Controller
+{
+    public function index()
+    {
+        $images = ImageUpload::latest()->get();
+
+        return view('welcome', compact('images'));
+    }
+
+    public function store()
+    {
+
+        if(! is_dir(public_path('/images'))) {
+            mkdir(public_path('/images'));
+        }
+
+        $images = Collection::wrap(request()->file('file'));
+
+        $images->each(function ($image) {
+            $basename = Str::random();
+            $original = $basename . '.' . $image->getClientOriginalExtension();
+            $thumbnail = $basename . '_thumb.' . $image->getClientOriginalExtension();
+
+            Image::make($image)
+              ->fit(250, 250)
+              ->save(public_path('/images/' . $thumbnail));
+
+            $image->move(public_path('/images') , $original);
+
+            ImageUpload::create([
+                'original' => '/images/'.$original,
+                'thumbnail' => '/images/'. $thumbnail,
+            ]);
+        });
+
+
+    }
+
+    public function destroy(ImageUpload $imageUpload)
+    {
+        // delete the files (original and thumbnail)
+        File::delete([
+            public_path($imageUpload->original),
+            public_path($imageUpload->thumbnail),
+        ]);
+
+        // delete the records in database
+        $imageUpload->delete();
+
+            // redirect
+        return redirect('/');
+    }
+}
